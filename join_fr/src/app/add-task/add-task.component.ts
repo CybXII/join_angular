24d30cce59fb04b333f827d4c4b3a6ac22288380
CybXII <luft.luft86@googlemail.com>
2024-07-29 14:20:29 +0200

    -editing inputfields

--- join_fr/src/app/add-task/add-task.component.ts
+++ join_fr/src/app/add-task/add-task.component.ts
@@ -1,16 +1,35 @@
-import {ChangeDetectionStrategy,Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card'; 
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatChipsModule } from '@angular/material/chips';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [MatCardModule,MatFormFieldModule, MatInputModule, MatSelectModule],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatRadioModule,
+    MatChipsModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss',
+  providers: [provideNativeDateAdapter()],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddTaskComponent {
-
+  todayDate: Date = new Date();
+  readonly categorys: string[] = ['Urgent', 'Medium', 'Low'];
+  contacts = new FormControl('');
+  contactsList: string[] = ['Alex', 'Lukas', 'Marcel,', 'Johannes'];
 }
