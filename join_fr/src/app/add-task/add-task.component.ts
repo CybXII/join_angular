62d7d2f7e10a524574083505c0445af5ffeca1f3
CybXII <luft.luft86@googlemail.com>
2024-07-30 02:04:16 +0200

    -edited addTaks component (subtasks and all inputfields

--- join_fr/src/app/add-task/add-task.component.ts
+++ join_fr/src/app/add-task/add-task.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, model, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
@@ -6,8 +6,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatRadioModule } from '@angular/material/radio';
-import { MatChipsModule } from '@angular/material/chips';
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { MatAutocompleteModule,MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 @Component({
   selector: 'app-add-task',
   standalone: true,
@@ -21,6 +25,8 @@ import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatChipsModule,
     FormsModule,
     ReactiveFormsModule,
+    MatIconModule,
+    MatAutocompleteModule
   ],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss',
@@ -32,4 +38,43 @@ export class AddTaskComponent {
   readonly categorys: string[] = ['Urgent', 'Medium', 'Low'];
   contacts = new FormControl('');
   contactsList: string[] = ['Alex', 'Lukas', 'Marcel,', 'Johannes'];
+
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  readonly currentSubtask = model('');
+  readonly subtasks = signal(['Lemon','Lemon','Lemon']);
+  readonly allSubtasks: string[] = [];
+
+  readonly announcer = inject(LiveAnnouncer);
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value) {
+      this.subtasks.update(subtasks => [...subtasks, value]);
+    }
+
+    // Clear the input value
+    this.currentSubtask.set('');
+  }
+
+  remove(subtask: string): void {
+    this.subtasks.update(subtasks => {
+      const index = subtasks.indexOf(subtask);
+      if (index < 0) {
+        return subtasks;
+      }
+
+      subtasks.splice(index, 1);
+      this.announcer.announce(`Removed ${subtask}`);
+      return [...subtasks];
+    });
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.subtasks.update(subtasks => [...subtasks, event.option.viewValue]);
+    this.currentSubtask.set('');
+    event.option.deselect();
+  }
+
 }
